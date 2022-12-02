ed78e2b2f2c8b76728fdfe732a1a14dbaa2dfee1
Mohamed Bakr <mohamed.bakr@bld.ai>
2022-12-02 15:13:16 +0200

    axios configuration

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import axios from 'axios'
 import './assets/main.css'
 
+axios.defaults.baseURL = 'https://test-api.storexweb.com/api/'
 const app = createApp(App)
 
 app.use(router)
