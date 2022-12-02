00dcce9a8d6f05730e21ac8ed83b4ca8e01a6875
Mohamed Bakr <mohamed.bakr@bld.ai>
2022-12-02 15:13:54 +0200

    registrations

---
+++ src/helper/AuthHelper.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+export default async function register(body) {
+
+   var data = new FormData();
+   data.append('name', body.name);
+   data.append('email', body.email);
+   data.append('password', body.password);
+
+   var config = {
+      method: 'post',
+      url: 'register',
+      headers: {
+         'Content-Type': 'multipart/form-data'
+      },
+      data: data
+   };
+
+   let res = axios(config)
+      .then(function (response) {
+         return JSON.stringify(response.data);
+      })
+      .catch(function (error) {
+         return error;
+      });
+   
+   return res;
+}
--- src/router/index.js
+++ src/router/index.js
@@ -6,16 +6,13 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: () => import('../views/HomeView.vue')
     },
-    // {
-    //   path: '/register',
-    //   name: 'register',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/RegisterView.vue')
-    // }
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/RegisterView.vue')
+    }
   ]
 })
 
