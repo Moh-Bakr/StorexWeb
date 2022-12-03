b5367386509868b63bbb04e907fa47e65ef01c16
Mohamed Bakr <mohamed.bakr@bld.ai>
2022-12-03 16:07:36 +0200

    card ui and functionality

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,46 +1,62 @@
 <script setup>
-import { destory } from '../helper/MovieHelper';
-import { reactive } from 'vue';
-import router from '../router';
-
+import { reactive } from "vue";
+import router from "../router";
 
 const props = defineProps(["data"]);
 
 let state = reactive({
-    alert: "",
+ alert: "",
 });
-
-function deleteHandler() {
-   destory(props.data.id).then((response) => {
-      console.log(response.status);
-      if (response.status == 'success') {
-         router.push({ name: "home" });
-      }
-      else {
-         state.alert = response.response.data.message;
-         console.log(state.alert);
-      }
-   });
-   
-}
 </script>
 
-
 <template>
-  <div class="card">
-    <img :src="'https://test-api.storexweb.com/' + props.data.image" />
-    <h1>{{ props.data.name }}</h1>
-    <h3>{{ props.data.description }}</h3>
-    <div>
-      <button @click="$emit('show', props.data)">Edit</button>
-      <button @click="deleteHandler">Delete</button>
-    </div>
+ <div class="card">
+  <img :src="props.data.image" />
+  <h3>{{ props.data.title }}</h3>
+  <p>{{ props.data.description }}</p>
+  <div class="actions-container">
+   <button class="edit-btn" @click="$emit('show', props.data)">Edit</button>
+   <button class="delete-btn" @click="$emit('delete', props.data.id)">
+    Delete
+   </button>
   </div>
+ </div>
 </template>
 
 <style scoped>
 img {
-  width: 100%;
-  height: 300px;
+ width: 100%;
+ height: 500px;
+}
+
+h3 {
+ text-align: center;
+ font-weight: bold;
+}
+
+.card {
+ background: rgb(207, 205, 205);
+ border-radius: 16px;
+ box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+ padding: 1rem;
+}
+
+.edit-btn {
+ background: #2e60cc;
+}
+
+.delete-btn {
+ background: #e74c3c;
+}
+
+img {
+ border-radius: 10px;
+}
+
+.actions-container {
+ margin-top: 10px;
+ display: grid;
+ gap: 5px;
+ grid-template-columns: 1fr 1fr;
 }
 </style>
