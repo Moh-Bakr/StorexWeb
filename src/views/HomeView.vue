534b387a6b3d9272f6ef6181fa97876c20f6661e
Mohamed Bakr <mohamed.bakr@bld.ai>
2022-12-04 12:49:23 +0200

    home ui & category filtration

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,40 @@
 <script setup>
-import { onMounted, reactive } from "vue";
-import index from "../helper/MovieHelper";
+import { onMounted, reactive, computed } from "vue";
+import { deleteMovie, getMovies } from "../helper/MovieHelper";
+import getCategories from "../helper/CategoryHelper";
 import Card from "../components/Card.vue";
 import Modal from "../components/Modal.vue";
 import axios from "axios";
+import router from "../router";
+import NavBar from "../components/Navbar.vue";
 
-const state = reactive({ movies: [], show: "", Modaldata: "" });
+const state = reactive({
+ movies: [],
+ categories: [],
+ show: "",
+ Modaldata: "",
+ selectedCategory: "",
+});
+
+const filteredMovies = computed(() => {
+ if (state.selectedCategory == "") {
+  return state.movies;
+ } else {
+  return state.movies.filter((movie) => {
+   return movie.category_id == state.selectedCategory;
+  });
+ }
+});
+
 onMounted(() => {
  if (localStorage.getItem("token") != undefined) {
-  let response = index().then((response) => {
-   state.movies = response.message;
+  getMovies().then((response) => {
+   state.movies = response.data;
   });
-//   console.log(state.movies);
+
+  getCategories().then((response) => {
+   state.categories = response.data;
+  });
  } else {
   router.push({ name: "login" });
  }
@@ -20,25 +43,68 @@ function hideModal() {
  state.show = false;
 }
 function show(data) {
- state.Modaldata = data;
+ state.Modaldata = {
+  ...data,
+  categories: state.categories,
+  action: data == "create" ? "Create" : "Edit",
+ };
  state.show = true;
+}
+
+function deleteHandler(id) {
+ deleteMovie(id).then((response) => {
+  console.log(response.status);
+  const position = state.movies.findIndex((movie) => movie.id == id);
+  state.movies.splice(position, 1);
+ });
 }
 </script>
 
 <template>
+ <NavBar @show="show('create')" />
+ <section>
+  <div class="categories-container">
+   <button
+    v-if="state.categories?.length > 0"
+    class="category-btn"
+    @click="state.selectedCategory = ''"
+    :class="{ 'active-category': state.selectedCategory == '' }"
+   >
+    All
+   </button>
+   <button
+    v-for="category in state.categories"
+    :key="category.id"
+    class="category-btn"
+    :class="{ 'active-category': category.id == state.selectedCategory }"
+    @click="state.selectedCategory = category.id"
+   >
+    {{ category.name }}
+   </button>
+  </div>
+ </section>
+ <p
+  v-if="!state.movies?.length || !state.categories?.length"
+  style="color: white; font-size: 20px; text-align: center; margin-top: 20px"
+ >
+  Loading ...
+ </p>
  <div class="movies">
-  <section v-for="item in state.movies" :key="item">
-   <Card v-if="item.image" @show="show" :data="item" />
+  <section v-for="item in filteredMovies" :key="item">
+   <Card v-if="item.image" @show="show" :data="item" @delete="deleteHandler" />
   </section>
 
   <Modal @hide="hideModal" :data="state.Modaldata" v-if="state.show" />
  </div>
 </template>
 
-<style scoped>
+<style>
+body {
+ background: url("../assets/bg.jpg") no-repeat center center fixed;
+}
 .movies {
  display: grid;
- grid-template-columns: 1fr;
+ grid-template-columns: 1fr 1fr;
 }
 
 @media (min-width: 1024px) {
@@ -47,5 +113,23 @@ function show(data) {
   grid-template-columns: 1fr 1fr 1fr 1fr;
   gap: 20px;
  }
+}
+
+.category-btn {
+ background: rgb(89, 89, 89);
+ margin: 5px;
+ width: 100px;
+ display: inline;
+}
+
+.categories-container {
+ padding: 10px;
+ display: flex;
+ justify-content: center;
+ align-items: center;
+}
+
+.active-category {
+ background: rgb(185, 104, 104);
 }
 </style>
