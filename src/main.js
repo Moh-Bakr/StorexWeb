db30c66879629e3e89bbcc4f278c61cb61bb31d1
Mohamed Bakr <mohamed.bakr@bld.ai>
2022-12-04 12:44:22 +0200

    added axios config & added ui styling and new logo

--- src/main.js
+++ src/main.js
@@ -4,7 +4,16 @@ import router from './router'
 import axios from 'axios'
 import './assets/main.css'
 
-axios.defaults.baseURL = 'https://test-api.storexweb.com/api/'
+axios.defaults.baseURL = 'https://storexweb.herokuapp.com/api/'
+
+// create axios instance
+const instance = axios.create({
+   baseURL: 'https://storexweb.herokuapp.com/api/',
+   headers: {
+      'Authorization': localStorage.getItem('token'),
+   }
+});   
+      
 const app = createApp(App)
 
 app.use(router)
